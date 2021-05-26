08c8104021ea8b928fa4cff04f34a8b4ba1f32c5
mawei <929198063@qq.com>
2021-05-26 16:41:11 +0800

    xiugai

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'mawei/swagger',
+  ),
+  'versions' => 
+  array (
+    'aferrandini/phpqrcode' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3c1c0454d43710ab5bbe19a51ad4cb41c22e3d46',
+    ),
+    'mawei/swagger' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
